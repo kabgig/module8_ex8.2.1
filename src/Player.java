15c43cf0d95858fe33cb69cb7f8814cf4a796ea5
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-11 19:15:49 +0400

    EX 8.2.1 DONE!

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Player extends Human{
 
     public Player(String name) {
@@ -6,7 +9,19 @@ public class Player extends Human{
 
     Backpack backpack = new Backpack();
 
-    public class Backpack {
+    public void take(String item) {
+        backpack.stuff.add(item);
+    }
 
+    public class Backpack {
+        ArrayList<String> stuff = new ArrayList<>();
+
+        @Override
+        public String toString() {
+            if (stuff.size() > 0){
+                String res = stuff.stream().collect(Collectors.joining(","));
+            return res + " in the backpack";}
+            else return "the backpack is empty";
+        }
     }
 }
