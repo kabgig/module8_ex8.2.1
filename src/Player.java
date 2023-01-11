94bf2a071c72431a953704c547593331acba0237
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-11 18:26:53 +0400

    First commit in the process

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player extends Human{
+
+    public Player(String name) {
+        super(name);
+    }
+
+    Backpack backpack = new Backpack();
+
+    public class Backpack {
+
+    }
+}
